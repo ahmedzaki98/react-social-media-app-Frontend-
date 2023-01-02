789e52b87a4b8d84d896b9a2f5337eb0db67389f
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-01-02 22:02:06 +0200

    Added Register Page Component

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="password" />
+            <input type="text" placeholder="Name" />
+            <button className="btn">Register</button>
+          </form>
+        </div>
+        <div className="register-left">
+          <h1>
+            <strong style={{ color: "#aa0014" }}>Z</strong>social
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span className="qus_span">Do you have an account?</span>
+          <Link to="/login">
+            <button className="btn">
+              <span>Login</span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
