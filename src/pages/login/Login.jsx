8c615e419b8ec854caed07c5b120ae4cecca5aa3
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-01-04 00:51:49 +0200

    updated login page header style

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -16,7 +16,9 @@ const Login = () => {
     <div className="login">
       <div className="card">
         <div className="left">
-          <h1>Hello World</h1>
+          <h1>
+            <strong style={{ color: "#aa0014" }}>Z</strong>social
+          </h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
