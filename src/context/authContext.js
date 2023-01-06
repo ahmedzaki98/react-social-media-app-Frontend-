8ec29efab6e01df1d20311934c3365b144c20712
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-01-06 17:20:40 +0200

    Fixed undefined bug

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,19 +1,25 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
+    JSON.parse(localStorage.getItem("user")) === "undefined"
+      ? JSON.parse(localStorage.getItem("user")) === null
+      : JSON.parse(localStorage.getItem("user"))
   );
 
-  const login = () => {
-    setCurrentUser({
-      id: 1,
-      name: "ahmed zaki",
-      profilePic:
-        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    });
+  const login = async (inputs) => {
+    const res = await axios.post(
+      "http://localhost:8800/api/auth/login",
+      inputs,
+      {
+        withCredentials: true,
+      }
+    );
+
+    setCurrentUser(res.data);
   };
 
   useEffect(() => {
