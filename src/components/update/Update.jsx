acabb8526ab9ff4e758a685bc9467dd244af2471
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-01-12 00:54:48 +0200

    Added Update component

--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -1,30 +1,34 @@
 import { useState } from "react";
 import { makeRequest } from "../../axios";
 import { useMutation, useQueryClient } from "react-query";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import CloseIcon from "@mui/icons-material/Close";
 import "./update.scss";
 
 const Update = ({ setOpenUpdate, user }) => {
-  const [cover, setCover] = useState(null);
-  const [profile, setProfile] = useState(null);
-  const [term, setTerm] = useState({
-    name: "",
-    city: "",
-    website: "",
+  //   const [cover, setCover] = useState(null);
+  //   const [profile, setProfile] = useState(null);
+  const [texts, setTexts] = useState({
+    email: user.email,
+    password: user.password,
+    name: user.name,
+    city: user.city,
+    website: user.website,
   });
 
-  const upload = async (file) => {
-    try {
-      const formData = new FormData();
-      formData.append("file", file);
-      const res = await makeRequest.post("/upload", formData);
-      return res.data;
-    } catch (er) {
-      console.log(er);
-    }
-  };
+  //   const upload = async (file) => {
+  //     try {
+  //       const formData = new FormData();
+  //       formData.append("file", file);
+  //       const res = await makeRequest.post("/upload", formData);
+  //       return res.data;
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
 
-  const onHandleChange = (e) => {
-    setTerm((prev) => ({ ...prev, [e.target.name]: [e.target.value] }));
+  const handleChange = (e) => {
+    setTexts((prev) => ({ ...prev, [e.target.name]: [e.target.value] }));
   };
 
   const queryClient = useQueryClient();
@@ -41,29 +45,108 @@ const Update = ({ setOpenUpdate, user }) => {
     }
   );
 
-  const onHandleClick = async (e) => {
+  const handleClick = async (e) => {
     e.preventDefault();
-    let coverUrl = user.coverPic;
-    let profileUrl = user.profilePic;
+    // //TODO: find a better way to get image URL
+    // let profileUrl;
+    // let coverUrl;
+    // profileUrl = profile ? await upload(profile) : user.profilePic;
+    // coverUrl = cover ? await upload(cover) : user.coverPic;
 
-    coverUrl = cover && (await upload(cover));
-    profileUrl = profile && (await upload(profile));
-
-    mutation.mutate({ ...term, coverPic: coverUrl, profilePic: profileUrl });
+    mutation.mutate({ ...texts });
     setOpenUpdate(false);
+    // setProfile(null);
+    // setCover(null);
   };
 
   return (
     <div className="update">
-      <form>
-        <input type="file" />
-        <input type="file" />
-        <input type="text" name="name" onChange={onHandleChange} />
-        <input type="text" name="city" onChange={onHandleChange} />
-        <input type="text" name="website" onChange={onHandleChange} />
-        <button onClick={onHandleClick}>Save</button>
-      </form>
-      <button onClick={() => setOpenUpdate(false)}>X</button>
+      <div className="wrapper">
+        <h1>Edit profile</h1>
+        <form>
+          <div className="files">
+            {/* <label htmlFor="profile">
+              <span>Profile Picture</span>
+              <div className="imgContainer">
+                <img
+                  src={
+                    profile
+                      ? URL.createObjectURL(profile)
+                      : "/upload/" + user.profilePic
+                  }
+                  alt=""
+                />
+                <CloudUploadIcon className="icon" />
+              </div>
+            </label>
+            <input
+              type="file"
+              id="profile"
+              style={{ display: "none" }}
+              onChange={(e) => setProfile(e.target.files[0])}
+            />
+            <label htmlFor="cover">
+              <span>Cover Photo</span>
+              <div className="imgContainer">
+                <img
+                  src={
+                    cover
+                      ? URL.createObjectURL(cover)
+                      : "/upload/" + user.coverPic
+                  }
+                  alt=""
+                />
+                <CloudUploadIcon className="icon" />
+              </div>
+            </label>
+            <input
+              type="file"
+              id="cover"
+              style={{ display: "none" }}
+              onChange={(e) => setCover(e.target.files[0])}
+            /> */}
+          </div>
+          <label>Email</label>
+          <input
+            type="text"
+            value={texts.email}
+            name="email"
+            onChange={handleChange}
+          />
+          <label>Password</label>
+          <input
+            type="text"
+            value={texts.password}
+            name="password"
+            onChange={handleChange}
+          />
+          <label>Name</label>
+          <input
+            type="text"
+            value={texts.name}
+            name="name"
+            onChange={handleChange}
+          />
+          <label>Country / City</label>
+          <input
+            type="text"
+            name="city"
+            value={texts.city}
+            onChange={handleChange}
+          />
+          <label>Website</label>
+          <input
+            type="text"
+            name="website"
+            value={texts.website}
+            onChange={handleChange}
+          />
+          <button onClick={handleClick}>Save</button>
+        </form>
+        <button className="close" onClick={() => setOpenUpdate(false)}>
+          <CloseIcon />
+        </button>
+      </div>
     </div>
   );
 };
